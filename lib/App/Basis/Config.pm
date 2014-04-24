3dfe331bc9e409325090520041439c217c2643c1
Kevin Mulholland <moodfarm@cpan.org>
2014-04-24 11:12:52 +0000

    Import of MOODFARM/App-Basis-0.5 from CPAN.
    gitpan-cpan-distribution: App-Basis
    gitpan-cpan-version:      0.5
    gitpan-cpan-path:         MOODFARM/App-Basis-0.5.tar.gz
    gitpan-cpan-author:       MOODFARM
    gitpan-cpan-maturity:     released

--- lib/App/Basis/Config.pm
+++ lib/App/Basis/Config.pm
@@ -3,7 +3,7 @@
 
 
 package App::Basis::Config;
-$App::Basis::Config::VERSION = '0.4';
+$App::Basis::Config::VERSION = '0.5';
 use 5.010;
 use warnings;
 use strict;
@@ -265,7 +265,7 @@ App::Basis::Config - Manage config YAML files in a simple manner
 
 =head1 VERSION
 
-version 0.4
+version 0.5
 
 =head1 SYNOPSIS
 
